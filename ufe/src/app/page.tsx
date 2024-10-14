08f3d0216ef2286f8a021261dcb7493c5dc602d4
Shaazgai <bzao.hover@gmail.com>
2024-10-14 14:57:34 +0800

    test

--- ufe/src/app/page.tsx
+++ ufe/src/app/page.tsx
@@ -1,101 +1,100 @@
+import Link from "next/link";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <>
+      <div className="container max-w-[400px] h-screen m-auto flex flex-col justify-start items-center gap-10 overflow-x-hidden bg-[#e6eef2]">
+        {/* <h1>Hello world</h1> */}
+        <video
+          src="/banner.mp4"
+          autoPlay
+          loop
+          muted
+          // playsInline
+          // controls
+          width="400"
+          height="600"
+        >
+          Your browser does not support the video tag.
+        </video>
+        <div className="text-gray-600">Урилганы текст </div>
+        <div className="flex gap-2 w-full">
+          <Link
+            href="#"
+            className="px-2 py-6 flex  text-centeritems-center justify-center bg-[#3d4466] rounded-lg w-[50%]"
           >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            <h5 className="text-2xl font-bold text-center text-gray-900 dark:text-white">
+              Шангрила Хүлээн авалтын танхим
+            </h5>
+          </Link>
+          <Link
+            href="#"
+            className="w-[50%] flex items-center justify-center px-2 py-6 bg-[#e6eef2] rounded-lg shadow-2xl border"
           >
-            Read our docs
-          </a>
+            <h5 className="flex items-center justify-center text-center text-2xl font-bold text-[#3d4466]">
+              2024.11.02
+            </h5>
+          </Link>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div className="p-6 bg-white w-full rounded-lg flex flex-col gap-4 text-[#3d4466]">
+          <p className="text-[#3d4466] font-bold text-xl uppercase py-3">
+            Хөтөлбөр
+          </p>
+          <span className="border-b pb-3">Зочдыг угтах үйл ажиллагаа </span>
+          <span className="border-b pb-3">Нээлтийн ажиллагаа </span>
+          <span className="border-b pb-3">
+            Санхүүчдийн ундраа шинэчилсэн клип{" "}
+          </span>
+          <span className="border-b pb-3">
+            Эдийн засаг, санхүүгийн тайлбар толийн нээлт{" "}
+          </span>
+          <span className="border-b pb-3 flex gap-1">
+            <p className="font-bold">&quot;Мөнгөн ирвэс&quot;</p> шагнал
+            гардуулах ёслол{" "}
+          </span>
+          <span className="border-b pb-3">UFE CENTURY FUND</span>
+          <span className="pb-4">Чөлөөт арга хэмжээ </span>
+        </div>
+        <div className="text-[#3d4466] font-bold text-2xl uppercase">
+          Онцлох арга хэмжээ{" "}
+        </div>
+        <div className="grid grid-cols-2 gap-2 p-2 border-b pb-5 border-b-[#3d4466] text-[#3d4466]">
+          <div className="w-[170px] h-[160px] border border-gray-800 rounded-xl">
+            Санхүүчдын ундраа{" "}
+          </div>
+          <div className="w-[170px] h-[160px] border border-gray-800 rounded-xl">
+            Санхүү эдийн засгийн тайлбар толь{" "}
+          </div>
+          <div className="w-[170px] h-[160px] border border-gray-800 rounded-xl">
+            Эдийн засаг, санхүүгийн тайлбар толийн нээлт{" "}
+          </div>
+          <div className="w-[170px] h-[160px] border border-gray-800 rounded-xl">
+            UFE Century fund{" "}
+          </div>
+        </div>
+        {/* <div className="bg-black w-full h-1"/> */}
+        <footer className="flex flex-col pb-10 gap-3 items-center">
+          <p className="text-[#3d4466]  font-medium text-xs">
+            Дэлгэрэнгүй мэдээлэл
+          </p>
+          <p className="text-[#3d4466] text-3xl font-bold">10000-2000</p>
+          <div className="mt-5">
+            {" "}
+            <Image
+              src={"/img/logo.png"}
+              alt="background"
+              width={80}
+              height={80}
+              // sizes="100%"
+              className="object-cover"
+            />
+          </div>
+          <p className="text-[#3d4466] text-xs">
+            All rights reserved, UFE © 2024
+          </p>
+        </footer>
+      </div>
+    </>
   );
 }
